b7ceaf4d54221e3abe613c15fe3d63fbca9529d7
Nelson Dias <nelson.dias@polarising.com>
2020-12-28 21:53:26 +0000

    added index.html page and respective indexController

---
+++ pet-clinic/pet-clinic-web/src/main/java/com/polarising/controllers/IndexController.java
@@ -0,0 +1,14 @@
+package com.polarising.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+	
+	@RequestMapping({"/", "", "index", "index.html"})
+	public String showIndexPage() {
+		return "index";
+	}
+}
